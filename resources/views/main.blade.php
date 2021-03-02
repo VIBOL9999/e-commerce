6b33ee5ff070fb5d5d1cdd2b654ac99725c1ca21
VIBOL Vlogger <47252393+VIBOL9999@users.noreply.github.com>
2021-03-02 23:21:38 +0700

    .

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -8,7 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
 </head>
 <body> -->
-@extends('layouts.header')
+@extends('layout.header')
 @section('content')
 
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
